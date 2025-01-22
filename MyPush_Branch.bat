f29d667feac1f9f486e474ce242d4e9b56e9cae3
峰花雪月-蔡孟峰 <smf1027@ms1.hinet.net>
2025-01-23 00:01:15 +0800

    Push Files -- MyBranch-2 ( 2025/01/23 週四,  0:01:15.04 )

--- MyPush_Branch.bat
+++ MyPush_Branch.bat
@@ -1,5 +1,5 @@
 
-echo "<br>Push Files -- %1 ( %date%, %time% )" >> README.md
+echo <br>Push Files -- %1 ( %date%, %time% ) >> README.md
 git add .
 git commit -m "Push Files -- %1 ( %date%, %time% )" 
 git push -u origin %1
