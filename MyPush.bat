c178fc72a315b92b3a07f10ef61ad99eb3b1ee1f
峰花雪月-蔡孟峰 <smf1027@ms1.hinet.net>
2025-01-22 23:42:32 +0800

    Push Files -- MyBranch-2 , 2025/01/22 週三, 23:42:32.63

--- MyPush.bat
+++ MyPush.bat
@@ -1,3 +1,3 @@
 git add .
-git commit -m "Push Files -- %date%, %time% " 
+git commit -m "Push Files" 
 git push -u origin main
--- MyPush_Branch.bat
+++ MyPush_Branch.bat
@@ -1,3 +1,4 @@
 git add .
-git commit -m "Push Files -- %1" 
+git commit -m "Push Files -- %1 , %date%, %time% " 
 git push -u origin %1
+
